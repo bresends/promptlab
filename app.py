5cdaf37325b9dc9e6b177e04349dd6b4efd36031
bruno <bruno.resende@gmx.com>
2025-06-05 17:04:31 -0300

    feat: flask basis

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from promptlab!")
+import os
+from flask import Flask, render_template
+from dotenv import load_dotenv
 
+load_dotenv()
+
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+app.secret_key = os.getenv('SECRET_KEY')
+
+@app.route('/')
+def index():
+    return render_template('index.j2')
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True, host='0.0.0.0', port=5000)
