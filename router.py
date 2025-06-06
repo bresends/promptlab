efbcbcd81d4a9f2412821027a0123beb561e381d
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2025-06-05 22:13:23 -0300

    feat: crud basics

--- router.py
+++ router.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from routes.index import index, set_data, get_data, clear_data, show_users
+from routes.index import index, set_data, get_data, clear_data, show_users, create_user
 from routes.auth import login
+
 
 def register_routes(app: Flask, db: SQLAlchemy):
     app.add_url_rule("/", view_func=index)
@@ -10,5 +11,5 @@ def register_routes(app: Flask, db: SQLAlchemy):
     app.add_url_rule("/get_data", view_func=get_data)
     app.add_url_rule("/clear_data", view_func=clear_data)
     app.add_url_rule("/users", view_func=show_users)
+    app.add_url_rule("/users/new", view_func=create_user, methods=["GET", "POST"])
     app.add_url_rule("/login", view_func=login, methods=["GET", "POST"])
-
--- routes/index.py
+++ routes/index.py
@@ -1,9 +1,12 @@
-from flask import render_template, session
+from flask import render_template, session, request, redirect, url_for, flash
 from models.user import User
+from app import db
+
 
 def index():
     """Main index page"""
     return render_template("index.j2", message="Hello, World!")
+
 
 def set_data():
     """Set data in session"""
@@ -27,14 +30,38 @@ def show_users():
     """Display all users from the database"""
     try:
         users = User.query.all()
-        if users:
-            user_list = [f"ID: {user.id}, Username: {user.username}, Email: {user.email}" for user in users]
-            message = "Users in database:<br>" + "<br>".join(user_list)
-        else:
-            message = "No users found in the database."
+        return render_template("users.j2", users=users)
     except Exception as e:
-        message = f"Error retrieving users: {str(e)}"
-    
-    return render_template("index.j2", message=message)
+        flash(f"Error retrieving users: {str(e)}")
+        return redirect(url_for("index"))
 
 
+def create_user():
+    """Create a new user"""
+    if request.method == "POST":
+        try:
+            username = request.form.get("username")
+            email = request.form.get("email")
+            password = request.form.get("password")
+
+            if not username or not email or not password:
+                flash("All fields are required!")
+                return redirect(url_for("index"))
+
+            new_user = User()
+            new_user.username = username
+            new_user.email = email
+            new_user.password = password
+
+            db.session.add(new_user)
+            db.session.commit()
+            flash(f"User {username} created successfully!")
+            return redirect(url_for("index"))
+
+        except Exception as e:
+            db.session.rollback()
+            flash(f"Error creating user: {str(e)}")
+            return redirect(url_for("index"))
+
+    # For GET requests, show the create user form
+    return render_template("create_user.j2")
